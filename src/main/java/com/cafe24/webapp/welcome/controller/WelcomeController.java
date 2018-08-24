38b268ec4f72cf48b4e19dc5c044054af3a2c5f3
silverhyuk <silverhyuk8@naver.com>
2018-08-24 20:44:06 +0900

    thymeleaf template + dialect layout 적용

--- src/main/java/com/cafe24/webapp/welcome/controller/WelcomeController.java
+++ src/main/java/com/cafe24/webapp/welcome/controller/WelcomeController.java
@@ -5,18 +5,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/welcome/")
 public class WelcomeController {
     @Autowired
     WelcomeService welcomeService;
 
-    @RequestMapping("/welcome")
+    /**
+     * @url localhost/welcome/hello.do
+     * @param model
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "hello.do", method = RequestMethod.GET)
     public String welcome(Model model) throws Exception {
         welcomeService.selectWelcomeList();
         model.addAttribute("name", "aa");
         model.addAttribute("title", "TITLE");
 
         return "welcome";
+    }
+    /**
+     * @url localhost/welcome/content.do
+     * @param model
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "content.do", method = RequestMethod.GET)
+    public String content(Model model) throws Exception {
+        return "content";
     }
 }
